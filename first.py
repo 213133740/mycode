7a5a9e358cec740ef9869da23adc24d3f61918ae
WeiJiaJu <WeiJiaJu@WeiJiaJu_PC>
2016-03-18 09:32:03 +0800

    第二次更新提交

--- first.py
+++ first.py
@@ -1,5 +1,15 @@
-#-*-coding:utf 8-*-
-print("hello")
+from __future__ import division
 import math
-print("2的12次方：",math.pow(2,12))
-print("try everything")
+D=2
+v=0.0098
+Re=2000
+rho=998
+V=Re*v/10000/(D/100)
+L=(math.pi/4)*((D/100)**2)*V
+print ("The required Largest Discharge",round(L*1000*60,3),"L/min")
+f=64/Re
+u=V*(math.sqrt(f/8))
+tou=rho*(u**2)
+print("tou=",round(tou,4),"Pa")
+    
+    
